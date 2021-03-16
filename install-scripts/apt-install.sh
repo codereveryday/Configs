4047f581caa77a03a428dcad83f88de3274c6b1e
Coder_Everyday <67515633+codereveryday@users.noreply.github.com>
2021-03-16 09:16:16 -0500

    Update apt-install.sh

--- install-scripts/apt-install.sh
+++ install-scripts/apt-install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #to install the lastest apt list
-sudo apt-get update -y
+sudo apt-get update -y 
 sudo apt-ger upgrade -y
 
 #to install my packages
 sudo apt-get install bluefish vokoscreen xmonad xmobar wireshark aircrack-ng leafpad xfce4-terminal pcmanfm nitrogen figlet neofetch virtualbox irssi
-sudo apt-get install firefox nano vim sudo openssh-server python3-pip git hydra -y
+sudo apt-get install firefox nano vim emacs sudo openssh-server python3-pip git hydra -y
 sudo apt-get install cupp dirbuster firejail 
 
 #makes sure your new packages are up to date
@@ -24,3 +24,4 @@ sudo apt --fix-broken install
 #prints end of program
 clear
 figlet "done"
+
